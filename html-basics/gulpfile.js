e343691a4e468b1592c19d4976fa28e6ea11b83c
Azad Asanali <azadxperias2012@gmail.com>
2017-08-05 12:41:01 +0530

    Initial commit and HTML Tables

---
+++ html-basics/gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp'),
+    connect = require('gulp-connect');
+
+var outputDir = './'; 
+var htmlSources = [ outputDir + '*.html'];
+
+gulp.task('watch', function() {
+    gulp.watch(htmlSources, ['html']);
+});
+
+gulp.task('connect', function() {
+    connect.server({
+        root: outputDir,
+        livereload: true
+    });
+});
+
+gulp.task('html', function() {
+    gulp.src(htmlSources)
+        .pipe(connect.reload());
+});
+
+gulp.task('default', ['html', 'connect', 'watch']);
