9adcd099bd06c401cce5597e4f45f6705a6dee12
Azad Asanali <azadxperias2012@gmail.com>
2017-08-09 07:50:54 +0530

    Added Stylesheet - Our first stylesheet lesson completed

---
+++ css-basics/gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp'),
+    connect = require('gulp-connect');
+
+var outputDir = './views/';
+var cssDir = './styles/';
+var htmlSources = [ outputDir + '*.html'];
+var cssSources = [ cssDir + '*.css'];
+
+gulp.task('watch', function() {
+    gulp.watch(htmlSources, ['html']);
+    gulp.watch(cssSources, ['css']);
+});
+
+gulp.task('connect', function() {
+    connect.server({
+        root: outputDir,
+        livereload: true
+    });
+});
+
+gulp.task('html', function() {
+    gulp.src(htmlSources)
+        .pipe(connect.reload());
+});
+
+gulp.task('css', function() {
+    gulp.src(cssSources)
+        .pipe(gulp.dest(outputDir + 'css'))
+        .pipe(connect.reload());
+});
+
+gulp.task('default', ['html', 'css', 'connect', 'watch']);
