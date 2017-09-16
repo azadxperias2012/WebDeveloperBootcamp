fa29f2a7cae7769203ec5b2f7a1381428c475043
Azad Asanali <azadxperias2012@gmail.com>
2017-09-16 16:17:08 +0530

    Added extra functionality and refactored code

--- JSBasics/TodoList/list.js
+++ JSBasics/TodoList/list.js
@@ -5,14 +5,37 @@ var input = prompt("What would you like to do?");
 while(input !== "quit") {
     // handle input
     if(input === "list") {
-        console.log(todos);
+        listTodos();
     } else if(input === "new") {
-        // Ask for new todo
-        var newTodo = prompt("Enter new todo");
-        // add to todos array
-        todos.push(newTodo);
+        addTodo();
+    } else if(input === "delete") {
+        deleteTodo();
     }
     // Ask again for new input
     input = prompt("What would you like to do?");
 }
 console.log("OK, YOU QUIT THE APP");
+
+function listTodos() {
+    console.log("*****************************");
+    for(var todoIndex in todos) {
+        console.log(todoIndex + ": " + todos[todoIndex]);
+    }
+    console.log("*****************************");
+}
+
+function addTodo() {
+    // Ask for new todo
+    var newTodo = prompt("Enter new todo");
+    // add to todos array
+    todos.push(newTodo);
+    console.log(newTodo + " has been added");
+}
+
+function deleteTodo() {
+    // Ask for index of todo to be deleted
+    var index = prompt("Enter the index of todo to delete");
+    // delete that todo
+    todos.splice(index, 1);
+    console.log("Deleted Todo");
+}
