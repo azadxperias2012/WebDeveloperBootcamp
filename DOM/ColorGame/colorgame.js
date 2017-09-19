6116888e220a66f57e3802ef529cc3c992980ca4
Azad Asanali <azadxperias2012@gmail.com>
2017-09-19 07:43:06 +0530

    Added randomized color pickers and game logic

--- DOM/ColorGame/colorgame.js
+++ DOM/ColorGame/colorgame.js
@@ -8,6 +8,38 @@ var colors = [
 ]
 
 var squares = document.querySelectorAll(".square");
+var pickedColor = pickColor();
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.getElementById("message");
+
+colorDisplay.textContent = pickedColor;
+
 for (var i = 0; i < squares.length; i++) {
-    squares[i].style.backgroundColor = colors[i];    
+    squares[i].style.backgroundColor = colors[i];
+
+    squares[i].addEventListener("click", function() {
+        // grab color of clicked square
+        var clickedColor = this.style.backgroundColor;
+        // compare color to pickedColor
+        if(clickedColor === pickedColor) {
+            messageDisplay.textContent = "Correct!";
+            changeColors(clickedColor);
+        } else {
+            this.style.backgroundColor = "#232323";
+            messageDisplay.textContent = "Try Again!";
+        }
+    });
+}
+
+function changeColors(color) {
+    // loop through all squares
+    squares.forEach(function(square) {
+        // change each color to match given color
+        square.style.backgroundColor = color;
+    });
+}
+
+function pickColor() {
+    var random = Math.floor(Math.random() * colors.length);
+    return colors[random];
 }
