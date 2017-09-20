1a1bb27d4aabbc341d7c3af0f5ec7a06352de81c
Azad Asanali <azadxperias2012@gmail.com>
2017-09-20 21:10:58 +0530

    jquery keypress event demo

--- jQuery/Demo/events.js
+++ jQuery/Demo/events.js
@@ -6,3 +6,10 @@ $("button").click(function() {
     var text = $(this).text();
     console.log("You clicked " + text);
 });
+
+$("input").keypress(function(event) {
+    if(event.which === 13) {
+        var text = $(this).val();
+        console.log("YOU HIT ENTER, text : " + text);
+    }
+});
