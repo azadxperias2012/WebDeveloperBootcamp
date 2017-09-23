479f7e89099fef85840636bbcf6f00b80a83859c
Azad Asanali <azadxperias2012@gmail.com>
2017-09-23 11:32:27 +0530

    Todo List : Deleting Todos

--- TODO_APP/assets/js/todos.js
+++ TODO_APP/assets/js/todos.js
@@ -2,3 +2,15 @@
 $("li").click(function() {
     $(this).toggleClass("completed");
 });
+
+// Click on X to delete Todo
+$("span").click(function(event) {
+    // here this refers to 'span' element
+    $(this).parent().fadeOut(500, function() {
+        // here this refers to 'li' element
+        $(this).remove();
+    });
+    // $(this).parent().remove();
+    // to stop bubbling of this event to parent
+    event.stopPropagation();
+});
