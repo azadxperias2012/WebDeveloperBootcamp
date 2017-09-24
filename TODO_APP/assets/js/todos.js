a66cb3cbe162092e51eb1381a053caf60f7070bb
Azad Asanali <azadxperias2012@gmail.com>
2017-09-24 11:16:25 +0530

    Todo List : Creating new todos

--- TODO_APP/assets/js/todos.js
+++ TODO_APP/assets/js/todos.js
@@ -1,16 +1,25 @@
 // Check Off Specific Todos By Clicking
-$("li").click(function() {
+$("ul").on("click", "li", function() {
     $(this).toggleClass("completed");
 });
 
 // Click on X to delete Todo
-$("span").click(function(event) {
+$("ul").on("click", "span", function(event) {
     // here this refers to 'span' element
     $(this).parent().fadeOut(500, function() {
         // here this refers to 'li' element
         $(this).remove();
     });
-    // $(this).parent().remove();
     // to stop bubbling of this event to parent
     event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event) {
+    if(event.which === 13) {
+        // grab new todo text from input
+        var todoText = $(this).val();
+        $(this).val("");
+        // create a new li and add to ul
+        $("ul").append("<li><span>X</span> " + todoText + "</li>");        
+    }
 });
